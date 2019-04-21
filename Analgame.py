0f2104ea010b5cdd599504c3877c38f2bbe95b54
MalachiWatkins <49846668+MalachiWatkins@users.noreply.github.com>
2019-04-21 15:15:24 -0700

    Add files via upload

---
+++ Analgame.py
@@ -0,0 +1,29 @@
+
+your_name = input("Enter your name: ")
+print("Do you like anal poundings " + your_name + "?")
+
+anal_answer = int(input("Your answer (1 or 0): "))
+if anal_answer<1:
+    print("Get out of here YOUR STRAIGHT PERSON")
+
+print("Enjoy this ;)")
+
+print("        ,-,-.")
+print("      ,'     `.")
+print("     /         \      I Could Really Use your tight ass to PUT MY SEMEN IN!!")
+print("    (___________)")
+print("      |(.) (.)|")
+print("      |  (_)  |")
+print("      |  ,-.  |")
+print("    ,'         `.")
+print("   / /|       |\ \ ")
+print("  / / |       | \ \ ")
+print(" / /  |       |  \ \ ")
+print(" ((   |       |   ))")
+print("     ,'`    ` `.")
+print("    / ` `  ` ` `\ ")
+print("   |``` `  ``` |")
+print("    `._`,'.`_,'")
+
+
+

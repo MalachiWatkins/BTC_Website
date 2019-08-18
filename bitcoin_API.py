96fb2ed6ea1428752b92fc5852510133fcc7de34
Malachi <rustadict1221@gmail.com>
2019-08-17 23:35:21 -0400

    WIP

---
+++ bitcoin_API.py
@@ -0,0 +1,28 @@
+import requests
+
+def get_btc(date = ""):
+    current_url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    date_url = "https://api.coindesk.com/v1/bpi/historical/close.json"
+    if date:
+        print("Date found:", date)
+        response = requests.get(date_url)
+        result = response.json()
+    else:
+        print("No date found!")
+        response = requests.get(current_url)
+        response_json = response.json()
+        result = response_json["bpi"]["USD"]["rate"]
+
+
+    return result
+# current https://api.coindesk.com/v1/bpi/currentprice.json
+
+#                               NO IF STATEMENTS
+#   use a library to interpret date when imputed 
+
+
+print(get_btc("ssssssssssssss"))
+#     if info == '1':
+#     current_usd = result["bpi"]["USD"]["rate"]
+#     print(current_usd)
+#     print(current_usd)

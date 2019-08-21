dca0e2c106906d89ab550d3eb2301d4a55be2d5d
Malachi Watkins <rustadict1221@gmail.com>
2019-08-21 16:49:41 -0400

    Working Sort of

--- bitcoin_API.py
+++ bitcoin_API.py
@@ -3,9 +3,10 @@ import requests
 def get_btc(date = ""):
     current_url = "https://api.coindesk.com/v1/bpi/currentprice.json"
     date_url = "https://api.coindesk.com/v1/bpi/historical/close.json"
+    user_date = {'start': date , 'end': '2016-08-17'}
     if date:
         print("Date found:", date)
-        response = requests.get(date_url)
+        response = requests.get(date_url, params=user_date)
         result = response.json()
     else:
         print("No date found!")
@@ -21,8 +22,9 @@ def get_btc(date = ""):
 #   use a library to interpret date when imputed
 
 
-print(get_btc(""))
+print(get_btc("2016-08-17"))
 #     if info == '1':
 #     current_usd = result["bpi"]["USD"]["rate"]
 #     print(current_usd)
 #     print(current_usd)
+#       ?start=2016-07-17&end=2016-08-17
---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+date = ("2016-07-17")
+payload = {'start': date , 'end': '2016-08-17'}
+r = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json', params=payload)
+
+print(r.url)
+
+
+#       ?start=2016-07-17&end=2016-08-17

9207e0ef8e5361b4024544c75082f279ebc39f7b
rustadict1221@gmail.com <rustadict1221@gmail.com>
2020-02-02 10:49:00 -0500

    WIP

--- Learning Scripts/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-date = ("2016-07-17")
-payload = {'start': date , 'end': date}
-r = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json', params=payload)
-
-print(r.url)
-
-
-#       ?start=2016-07-17&end=2016-08-17
